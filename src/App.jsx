eca144bbd6fd8fb760fb1b3656b8e98a333c81a1
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-05 15:54:11 +0100

    initialising the file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import {
+	Footer,
+	Blog,
+	Possibility,
+	Features,
+	WhatGPT3,
+	Header,
+} from "./containers";
+
+import { Brand, Cta, Navbar } from "./components";
 
 const App = () => {
-  return (
-    <div><h1>header</h1></div>
-  )
-}
+	return (
+		<div className="App">
+			<div className="gradient__bg">
+				<Navbar />
+				<Header />
+			</div>
+			<Brand />
+			<WhatGPT3 />
+			<Features />
+			<Possibility />
+			<Cta />
+			<Blog />
+			<Footer />
+		</div>
+	);
+};
 
-export default App
+export default App;

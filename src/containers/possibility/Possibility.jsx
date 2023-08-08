cafe0f59fbf520e09a4e4d2575f953e141f4ed27
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-09 00:34:30 +0100

    started the possibility component

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import "./possibility.css"
+import React from "react";
+import possibilityImage from "../../assets/possibility.png";
+import "./possibility.css";
 
 const Possibility = () => {
-  return (
-    <div>Possibility</div>
-  )
-}
+	return (
+		<div className="gpt3__possibility section__padding " id="possibility">
+			<div className="gpt3__possibility-image">
+				<img src={possibilityImage} alt="possibilityImage" />
+			</div>
+		</div>
+	);
+};
 
-export default Possibility
+export default Possibility;

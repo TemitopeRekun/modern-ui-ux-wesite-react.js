fd31013387ccd7edf1b321083a3a498883267ec7
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-11 01:42:40 +0100

    added hover effect

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -5,7 +5,7 @@ import { Article } from "../../components";
 
 const Blog = () => {
 	return (
-		<div className="gpt3__blog  section__padding">
+		<div className="gpt3__blog  section__padding" id="blog">
 			<div className="gpt3__blog-heading">
 				<h1 className="gradient__text">
 					A lot is happening, We are blogging about it.

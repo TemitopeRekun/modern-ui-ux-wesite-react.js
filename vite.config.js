c0250ca35cdde96a568127f065644197c16ad1fb
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-12 12:33:00 +0100

    made canges to the deployment files

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default 
-  defineConfig({
-  base: "/gpt3_",
-  plugins: [react()],
-})
+export default defineConfig({
+	base: "/modern-ui-ux-wesite-react.js",
+	plugins: [react()],
+});
